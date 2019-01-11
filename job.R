3c68aad7cc975dda58f8f10cbcff59c7a6376566
Jonathan McPherson <jonathan@rstudio.com>
2019-01-10 16:10:59 -0800

    updates

---
+++ job.R
@@ -0,0 +1,41 @@
+
+# This file is an example of a long-running job.
+
+# Step 1 --------------------------------------------------
+
+message("Reticulating splines...")
+Sys.sleep(1)
+Sys.sleep(1)
+Sys.sleep(1)
+Sys.sleep(1)
+
+# Step 2 --------------------------------------------------
+
+message("Performing deviation analysis...")
+Sys.sleep(1)
+Sys.sleep(1)
+
+# Step 3 --------------------------------------------------
+
+message("Factoring dark matter...")
+Sys.sleep(1)
+Sys.sleep(1)
+Sys.sleep(1)
+Sys.sleep(1)
+Sys.sleep(1)
+Sys.sleep(1)
+
+# Step 4 --------------------------------------------------
+
+message("Computing derivatives of scalars...")
+Sys.sleep(1)
+Sys.sleep(1)
+Sys.sleep(1)
+
+
+# Step 5 --------------------------------------------------
+
+message("Task complete.")
+
+mtcars <- data(mtcars)
+
